111dba8385b41cf1f59d680db002f0fafdc48a61
ricardobr25 <113959643+RicardoBr25@users.noreply.github.com>
2023-11-08 17:56:55 -0500

    Video 27 Validadores asintonos

--- formularios/src/app/services/validadores.service.ts
+++ formularios/src/app/services/validadores.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +20,18 @@ export class ValidadoresService {
     return {noHerrera: null};
   }
 
-  passwordsIguales(){
+  passwordsIguales( pass1Name: string, pass2Name: string){
 
+    return(FormGroup: FormGroup) =>{
+      const pass1Control = FormGroup.controls[pass1Name];
+      const pass2Control = FormGroup.controls[pass2Name];
+
+      if( pass1Control.value === pass2Control.value){
+        pass2Control.setErrors(null);
+      }else {
+        pass2Control.setErrors({noEsIgual: true});
+      }
+
+    }
   }
 }
