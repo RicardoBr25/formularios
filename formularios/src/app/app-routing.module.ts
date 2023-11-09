17f43d50766e1d05cdd5dae52892c1d60e9e7b10
ricardobr25 <113959643+RicardoBr25@users.noreply.github.com>
2023-11-08 23:11:53 -0500

    Video 28

--- formularios/src/app/app-routing.module.ts
+++ formularios/src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { TemplateComponent } from './pages/template/template.component';
 import { ReactiveComponent } from './pages/reactive/reactive.component';
 
 const routes: Routes = [
-  { path:'template', component: TemplateComponent},
-  { path:'reactivo', component: ReactiveComponent},
-  { path:'**', pathMatch: 'full', redirectTo:'template'}
+  { path: 'template', component: TemplateComponent },
+  { path: 'reactivo', component: ReactiveComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'template' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- formularios/src/app/services/validadores.service.ts
+++ formularios/src/app/services/validadores.service.ts
@@ -1,37 +1,55 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
 
+interface ErrorValidate {
+  [s: string]: boolean | any;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ValidadoresService {
+  constructor() {}
 
-  constructor() { }
+  existeUsuario(
+    control: FormControl
+  ): Promise<ErrorValidate> | Observable<ErrorValidate> {
 
-  noHerrera(control: FormControl ): {[s: string ]: boolean | any } {
-
-
-    if (control.value?.toLowerCase() === 'herrera') {
-      return {
-        noHerrera: true
-      }
+    if (!control.value) {
+      return Promise.resolve(null!);
     }
 
-    return {noHerrera: null};
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'strider') {
+          resolve({ existe: true });
+        } else {
+          resolve(null!);
+        }
+      }, 3500);
+    });
   }
 
-  passwordsIguales( pass1Name: string, pass2Name: string){
-
-    return(FormGroup: FormGroup) =>{
-      const pass1Control = FormGroup.controls[pass1Name];
-      const pass2Control = FormGroup.controls[pass2Name];
-
-      if( pass1Control.value === pass2Control.value){
-        pass2Control.setErrors(null);
-      }else {
-        pass2Control.setErrors({noEsIgual: true});
-      }
-
+  noHerrera(control: FormControl): ErrorValidate {
+    if (control.value?.toLowerCase() === 'herrera') {
+      return {
+        noHerrera: true,
+      };
     }
+
+    return { noHerrera: null };
+  }
+
+  passwordsIguales(pass1Name: string, pass2Name: string) {
+    return (formGroup: FormGroup) => {
+      const pass1Control = formGroup.controls[pass1Name];
+      const pass2Control = formGroup.controls[pass1Name];
+
+      if (pass1Control.value === pass2Control.value) {
+        pass2Control.setErrors(null);
+      } else {
+        pass2Control.setErrors({ noEsIgual: true });
+      }
+    };
   }
 }
